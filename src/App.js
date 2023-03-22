05f28ce850999efa51e9cc3423e82c8f8339cc7c
Sinziana Sovejanu <27846889+SinzianaS@users.noreply.github.com>
2023-03-22 15:56:26 +0200

    added side menu with routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import "antd/dist/reset.css";
+import React from "react";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  UserOutlined,
+  SmileOutlined,
+  IdcardOutlined,
+  CrownOutlined,
+  TeamOutlined,
+} from "@ant-design/icons";
 import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <Menu
+        onClick={({ key }) => {
+           navigate(key);
+        }}
+        items={[
+          { label: "Home", key: "/", icon: <HomeOutlined /> },
+          { label: "Dentists", key: "/dentists", icon: <IdcardOutlined /> },
+          { label: "Technicians", key: "/dentaltechnicians", icon: <TeamOutlined />},
+          { label: "Patients", key: "/patients", icon: <SmileOutlined /> },
+          { label: "Dental works", key: "/dentalworks", icon: <CrownOutlined />},
+          { label: "Login", key: "/authentication", icon: <UserOutlined /> },
+        ]}
+      ></Menu>
+      <Content />
+    </div>
+  );
+}
+function Content() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>}></Route>
+        <Route path="/dentists" element={<div>Dentists</div>}></Route>
+        <Route path="/dentaltechnicians" element={<div>Dental technicians</div>}></Route>
+        <Route path="/patients" element={<div>Patients</div>}></Route>
+        <Route path="/dentalworks" element={<div>Dental works</div>}></Route>
+        <Route path="/authentication" element={<div>Login</div>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Patients from "./pages/patients/Patients";
-import PatientExtraDetails from "./pages/patients/PatientExtraDetails";
-import Authentication from "./pages/authentication/Authentication";
+import { BrowserRouter } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "patients",
-    element: <Patients />,
-  },
-  {
-    path: "patients/:id",
-    element: <PatientExtraDetails />,
-  },
-  {
-    path: "authentication",
-    element: <Authentication />,
-  },
-
-]);
-
-root.render(
+ ReactDOM.render(
   <React.StrictMode>
-    <nav>
-      <ul>
-        <li>
-          <a href="/patients">Patients</a>
-        </li>
-        <li>
-          <a href="/authentication">Authentication</a>
-        </li>
-      </ul>
-    </nav>
-    <RouterProvider router={router}>
+    <BrowserRouter>
       <App></App>
-    </RouterProvider>
-  </React.StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
